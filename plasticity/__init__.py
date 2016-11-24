6803f33c224f30d933abb746fcb9caa09b5975a4
Brian Blais <bblais@gmail.com>
2016-11-24 10:07:56 -0500

    2 to 3 conversion

--- plasticity/__init__.py
+++ plasticity/__init__.py
@@ -1,11 +1,11 @@
-import version
+from . import version
 
 __version__=version.version
 
-import utils
-from run import run
-import dialogs
+from . import utils
+from .run import run
+from . import dialogs
 import train
-import run_sim as run_sim_mod
-from run_sim import run_sim
+from . import run_sim as run_sim_mod
+from .run_sim import run_sim
 
